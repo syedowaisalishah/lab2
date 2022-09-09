3630216f87917d003be922e2d37c310ece068cd8
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2022-09-10 01:46:29 +0500

    Add files via upload

---
+++ src/test/scala/practice/OTEST.scala
@@ -0,0 +1,15 @@
+// package practice
+// import chisel3._
+// import org.scalatest._
+// import chiseltest._
+// class DECODERTEST extends FreeSpec with ChiselScalatestTester{
+//     "mux testing" in {
+//         test(new Decoder2to4()){c=>
+//         c.io.in.poke(2.U)
+//         c.io.out.expect(1.U)
+//         }
+
+//     }
+
+
+// }
